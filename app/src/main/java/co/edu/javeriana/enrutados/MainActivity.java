f8d55357b0849b339cadd0044205a73dc5393498
Jose Quintero <joseantonioq9@gmail.com>
2020-03-15 14:40:36 -0500

    Setting up main activity layout

--- app/src/main/java/co/edu/javeriana/enrutados/MainActivity.java
+++ app/src/main/java/co/edu/javeriana/enrutados/MainActivity.java
@@ -3,12 +3,38 @@ package co.edu.javeriana.enrutados;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button forgotPassword, login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Inflate
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        forgotPassword = (Button) findViewById(R.id.forgot_password);
+        login = (Button) findViewById(R.id.login);
+
+        //Add events
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Send to ldap password recovery
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Validate if technician or coordinator and call new HomeActivity
+            }
+        });
     }
 }
