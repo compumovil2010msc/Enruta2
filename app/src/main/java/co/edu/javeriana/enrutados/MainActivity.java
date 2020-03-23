d030dc0062456d2feec1b0991b54575808f69c0e
Jose Quintero <joseantonioq9@gmail.com>
2020-03-23 09:38:40 -0500

    Added DrawerActivity as the home Sorted strings by key Changed color scheme

--- app/src/main/java/co/edu/javeriana/enrutados/MainActivity.java
+++ app/src/main/java/co/edu/javeriana/enrutados/MainActivity.java
@@ -2,11 +2,14 @@ package co.edu.javeriana.enrutados;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,8 +38,41 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO: Validate if technician or coordinator and call new HomeActivity
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                if (user.isEmpty()) {
+                    username.setError(getString(R.string.error_empty_field));
+                } else {
+                    validateUser(user, pass);
+                }
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    private void validateUser (String user, String pass) {
+        SharedPreferences sharedPref = getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        if (user.equals("coord")) {
+            editor.putString(getString(R.string.preference_user_key), user);
+            editor.putString(getString(R.string.preference_acc_key), getString(R.string.preference_acc_coord));
+            editor.apply();
+            Intent intent = new Intent(this, DrawerActivity.class);
+            startActivity(intent);
+        } else if (user.equals("tech")) {
+            editor.putString(getString(R.string.preference_user_key), user);
+            editor.putString(getString(R.string.preference_acc_key), getString(R.string.preference_acc_tech));
+            editor.apply();
+            Intent intent = new Intent(this, DrawerActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, R.string.error_incorrect_login, Toast.LENGTH_SHORT).show();
+        }
     }
 }
