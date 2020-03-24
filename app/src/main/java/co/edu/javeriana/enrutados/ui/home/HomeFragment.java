a9e2dcb2c36217f2291f02bfc38d7902ff52a4bf
Jose Quintero <joseantonioq9@gmail.com>
2020-03-24 01:32:32 -0500

    Home activity done

--- app/src/main/java/co/edu/javeriana/enrutados/ui/home/HomeFragment.java
+++ app/src/main/java/co/edu/javeriana/enrutados/ui/home/HomeFragment.java
@@ -11,23 +11,53 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import co.edu.javeriana.enrutados.R;
+import co.edu.javeriana.enrutados.model.Task;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    RecyclerView listOfTasks;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter taskAdapter;
+
+    List <Task> tasks;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        listOfTasks = (RecyclerView) root.findViewById(R.id.list_of_tasks);
+        listOfTasks.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(root.getContext());
+        listOfTasks.setLayoutManager(layoutManager);
+        LoadData(root);
+
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
             }
         });
         return root;
+    }
+
+    private void LoadData(View v) {
+        tasks = new ArrayList<Task>();
+        taskAdapter = new TaskAdapter(v.getContext(), tasks);
+        listOfTasks.setAdapter(taskAdapter);
+
+        tasks.add(new Task("Puerto Claver", "Instalación de punto de red y mantenimiento preventivo", false));
+        tasks.add(new Task("Margento", "Instalación de antena y puntos de red", false));
+        tasks.add(new Task("Cuturú", "Reparación de teléfonos satelitales", false));
+        tasks.add(new Task("Palanca", "Insertar caja de energía", true));
+        tasks.add(new Task("Colorado", "Instalación de punto de red y mantenimiento preventivo", true));
     }
 }
---
+++ app/src/main/java/co/edu/javeriana/enrutados/ui/home/TaskAdapter.java
@@ -0,0 +1,80 @@
+package co.edu.javeriana.enrutados.ui.home;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import co.edu.javeriana.enrutados.R;
+import co.edu.javeriana.enrutados.model.Task;
+
+public class TaskAdapter extends RecyclerView.Adapter <TaskAdapter.MyViewHolder>{
+
+    private List<Task> tasks;
+    private Context context;
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView taskTitle;
+        TextView taskDetail;
+        ImageView doneStatus;
+        public MyViewHolder (View v) {
+            super(v);
+            taskTitle = (TextView) v.findViewById(R.id.task_title);
+            taskDetail = (TextView) v.findViewById(R.id.task_title);
+            doneStatus = (ImageView) v.findViewById(R.id.done_status);
+        }
+    }
+
+    public TaskAdapter(Context context, List<Task> tasks) {
+        this.context = context;
+        this.tasks = tasks;
+    }
+
+    void startTaskActivity(int i) {
+        //TODO: Add tab activity
+//        Intent intent = new Intent(context, DetailActivity.class);
+//        intent.putExtra("NAME", products.get(i).getName());
+//        intent.putExtra("DESCRIPTION", products.get(i).getDescripcion());
+//        intent.putExtra("PRICE", products.get(i).getPrice());
+//        intent.putExtra("IMAGE", products.get(i).getImagepath());
+//        context.startActivity(intent);
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(context).inflate(R.layout.task_item, viewGroup, false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+        final int position = i;
+        myViewHolder.taskTitle.setText(tasks.get(i).getTitle());
+        myViewHolder.taskDetail.setText(tasks.get(i).getDescription());
+        if (tasks.get(i).isDone()) {
+            myViewHolder.doneStatus.setImageResource(R.drawable.ic_check_circle_black_24dp);
+            myViewHolder.doneStatus.setColorFilter(R.color.positive_color);
+        }
+        myViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTaskActivity(position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return tasks.size();
+    }
+
+}
