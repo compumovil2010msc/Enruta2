37b46ab199e658a7f2609deda415050e78382337
miguel <ing.m.cortes@gmail.com>
2020-05-03 07:52:05 -0500

    Added proxy and artifacts for backend

---
+++ app/src/main/java/co/edu/javeriana/enrutados/model/Route.java
@@ -0,0 +1,43 @@
+package co.edu.javeriana.enrutados.model;
+
+import java.util.List;
+
+public class Route {
+    private Long id;
+    private String name;
+    private String createdAt;
+
+    private List<Point> points;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public List<Point> getPoints() {
+        return points;
+    }
+
+    public void setPoints(List<Point> points) {
+        this.points = points;
+    }
+}
---
+++ app/src/main/java/co/edu/javeriana/enrutados/model/User.java
@@ -0,0 +1,31 @@
+package co.edu.javeriana.enrutados.model;
+
+public class User {
+    private Long id;
+    private String email;
+    private String role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
