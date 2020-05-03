05a92ee4c209f0e33679cefd73e8e910a570dacb
miguel <ing.m.cortes@gmail.com>
2020-05-03 15:44:36 -0500

    Added Location Events on Map Activity
    Also added marker options for completed points

--- app/src/main/java/co/edu/javeriana/enrutados/MapsActivity.java
+++ app/src/main/java/co/edu/javeriana/enrutados/MapsActivity.java
@@ -1,23 +1,40 @@
 package co.edu.javeriana.enrutados;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import co.edu.javeriana.enrutados.model.Point;
 import co.edu.javeriana.enrutados.model.Route;
 
+import static co.edu.javeriana.enrutados.Utils.*;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    public static final int REQUEST_CODE = 1;
+    public static final String REQUEST_REASON = "Location required in order to provide directions";
     private GoogleMap mMap;
 
+    LocationManager locationManager;
+    LocationListener locationListener;
     Route activeRoute;
 
     @Override
@@ -30,8 +47,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         activeRoute = (Route) getIntent().getSerializableExtra("route");
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListenerHandle();
+
+        startLocationService();
     }
 
+    private void startLocationService() {
+        if (!checkForPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            requestPermission(this, Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_REASON, REQUEST_CODE);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (checkForPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -46,14 +86,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         Integer i = 0;
+
         for (Point point : activeRoute.getPoints()) {
             LatLng location = new LatLng(point.getLatitude(), point.getLongitude());
-            mMap.addMarker(new MarkerOptions().position(location).title(point.getName()));
+            MarkerOptions makerOptions = new MarkerOptions().position(location).title(point.getName());
+
+            if (point.getFinished())
+                makerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+
+            mMap.addMarker(makerOptions);
 
             if (i.equals(0))
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12));
 
             i++;
+        }
+    }
+
+    private class LocationListenerHandle implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.i("Location", location.toString());
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
         }
     }
 }
